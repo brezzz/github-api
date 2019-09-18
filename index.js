c51e3312df518a1919c795b046b15c593872336f
brezzz <brezzz@users.noreply.github.com>
2019-09-17 20:20:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ function displayResults(responseJson) {
     console.log('Username is: ' + responseJson[0].owner.login);
     console.log(`${responseJson[0].owner.login}\'s github url is:`  + responseJson[0].owner.html_url);
     
-
- let header = 
+ let user = responseJson[0].owner.login;
+ let profile = 
       `<li><h3><a href="${responseJson[0].owner.html_url}">${responseJson[0].owner.login}</a></h3>
       <img src='${responseJson[0].owner.avatar_url}'>
       <a href="${responseJson[0].owner.html_url}"><p>${responseJson[0].owner.html_url}</p></a>
-       
-
-
       </li>
-       <h3>Repos:</h3> `   
-    $('#results-list').append(header);
+      
+      <h4 class='repos'><a href ='https://github.com/${user}?tab=repositories'> Repos</a>: ${responseJson.length}</h4> `   
+    $('#results-list').append(profile);
    for (let i = 0; i < responseJson.length; i++){   
      $('#results-list').append(
-      `<p>${responseJson[i].name}</p>`)
-      // maybe add <p>${responseJson[i].description}</p>  
+`
+      <ul class= 'repos'><span id='name'><a href='${responseJson[i].html_url}'>${responseJson[i].name}</a></span>:
+      <li>"${responseJson[i].description}"</li></ul>`)
+       
       
     }
   //display the results section
@@ -40,6 +40,8 @@ function getUser(username) {
 
   const url = `https://api.github.com/users/${username}/repos`;
 console.log(url);
+ $('#js-error-message').html("");
+$('#results-list').html("");
 fetch(url)
     .then(response => {
       if (response.ok) {
@@ -51,6 +53,8 @@ fetch(url)
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
+
+    
 }
 
 function watchForm() {
